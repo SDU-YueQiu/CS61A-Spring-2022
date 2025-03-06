a50218f95eae3257fd05356724232d65c7346a97
Yueq2003 <iop18653346184@hotmail.com>
2025-03-07 00:10:55 +0800

    -完成lab1

---
+++ Labs/lab01/parsons_probs/k_occurrence.py
@@ -0,0 +1,15 @@
+def k_occurrence(k, num):
+    """
+    >>> k_occurrence(5, 10)  # .Case 1
+    0
+    >>> k_occurrence(5, 5115)  # .Case 2
+    2
+    >>> k_occurrence(0, 100)  # .Case 3
+    2
+    >>> k_occurrence(0, 0)  # .Case 4
+    0
+    """
+    "*** YOUR CODE HERE ***"
+    if num == 0:
+        return 0
+    return str(num).count(str(k))
